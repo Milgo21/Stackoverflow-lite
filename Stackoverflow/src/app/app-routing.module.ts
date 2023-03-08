72a981e3bd3afbb72292b30a5f04f0895a4db3e2
Milgo21 <felixmilgo21@gmail.com>
2023-03-08 18:57:23 +0300

    Completed fronteend

--- Stackoverflow/src/app/app-routing.module.ts
+++ Stackoverflow/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', loadComponent:() => import('./components/home-page/home-page.component').then(h=>h.HomePageComponent)},
+  {path:'login', loadComponent:() => import('./components/login/login.component').then(l=>l.LoginComponent)},
+  {path:'register', loadComponent:() => import('./components/register/register.component').then(r=>r.RegisterComponent)},
+  {path:'reset', loadComponent:() => import('./components/forgotpassword/forgotpassword.component').then(f=>f.ForgotpasswordComponent)},
+  {path:'posts', loadComponent:() => import('./components/posts/posts.component').then(p=>p.PostsComponent)},
+  {path:'id', loadComponent:() => import('./components/posts/singlepost/singlepost.component').then(s=>s.SinglepostComponent)},
+  {path:'ask', loadComponent:() => import('./components/questions/askquestion/askquestion.component').then(a=>a.AskquestionComponent)},
+  {path:'update', loadComponent:() => import('./components/questions/updatequestion/updatequestion.component').then(u=>u.UpdatequestionComponent)},
+  {path:'profile', loadComponent:() => import('./components/profile/profile.component').then(p=>p.ProfileComponent)},
+  {path:'admin', loadComponent:() => import('./components/admin/admin.component').then(a=>a.AdminComponent),
+    children :[
+      {path:'', loadComponent:() => import('./components/admin/questions/questions.component').then(q=>q.QuestionsComponent)},
+      {path:'users', loadComponent:() => import('./components/admin/users/users.component').then(u=>u.UsersComponent)}
+    ]
+  },
+  {path:'**', loadComponent:() => import('./components/page-not-found/page-not-found.component').then(n=>n.PageNotFoundComponent)},
+
+
+
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Stackoverflow/src/app/app.module.ts
+++ Stackoverflow/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { RegisterComponent } from "./components/register/register.component";
 import { PostsComponent } from "./components/posts/posts.component";
 import { SinglepostComponent } from "./components/posts/singlepost/singlepost.component";
 import { ProfileComponent } from "./components/profile/profile.component";
+import { AdminComponent } from "./components/admin/admin.component";
 
 
 @NgModule({
@@ -24,16 +26,9 @@ import { ProfileComponent } from "./components/profile/profile.component";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HeaderComponent,
         FooterComponent,
-        HomePageComponent,
-        LoginComponent,
-        AskquestionComponent,
-        UpdatequestionComponent,
-        RegisterComponent,
-        PostsComponent,
-        SinglepostComponent,
-        ProfileComponent
+        HeaderComponent,
+        HttpClientModule
     ]
 })
 export class AppModule { }
